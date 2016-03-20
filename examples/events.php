ee4bf3a8fda4693375e2f7d88ccef579d2244ea0
Sergey Protasevich <sergey.p@gmx.com>
2016-03-20 21:31:28 +0300

    Unite event examples, rewrite examples for new API

--- examples/events.php
+++ examples/events.php
@@ -5,8 +5,19 @@ require_once __DIR__.'/../vendor/autoload.php';
 class Observer implements \SplObserver {
     public function update(\SplSubject $event) {
         // $event instanceof WeakArray\Event;
-        // see WeakArray\Event class to view all available methods and Event::TYPE_* constants
-        printf("Event %s, key %s\n", $event->getType(), $event->getKey());
+        // see WeakArray\Event class to view all available methods and Event::* constants
+        switch ($event->getType()) {
+            case WeakArray\Event::OBJECT_SET:
+                $event_str = 'set';
+                break;
+            case WeakArray\Event::OBJECT_UNSET:
+                $event_str = 'unset';
+                break;
+            case WeakArray\Event::OBJECT_DESTRUCTED:
+                $event_str = 'destructed';
+                break;
+        }
+        printf("Object %s, key %s\n", $event_str, $event->getKey());
     }
 }
 
@@ -23,7 +34,8 @@ $weak_array['foo'] = $foo;
 $weak_array['bar'] = $bar;
 $weak_array['baz'] = $baz;
 
-unset($weak_array['foo']);
+unset($foo);
+
 unset($weak_array['bar']);
 unset($weak_array['baz']);
 
@@ -31,9 +43,9 @@ unset($weak_array['baz']);
 
 EXPECTED OUTPUT:
 
-Event 1, key foo
-Event 1, key bar
-Event 1, key baz
-Event 2, key foo
-Event 2, key bar
-Event 2, key baz
+Object set, key foo
+Object set, key bar
+Object set, key baz
+Object destructed, key foo
+Object unset, key bar
+Object unset, key baz
